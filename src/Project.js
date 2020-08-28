bab08158e1c83b2f2bad7d30842fee4d3ef4bae8
Abrahamcepe <abrahamgolf@gmail.com>
2020-08-28 13:45:17 -0500

    Added function addTodo which adds todos

--- src/Project.js
+++ src/Project.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { db } from "./firebase";
+import firebase from "firebase";
 import { Button, FormControl, InputLabel, Input, List } from "@material-ui/core";
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import './css/Project.css'
@@ -11,18 +12,25 @@ function Project(props) {
     const [todos, setTodos] = useState([]);
     
     useEffect(() => {
-      db.collection("projects")
+      db.collection('projects')
         .doc(props.projectID)
-        .collection("todos")
-        .orderBy("timestamp", "desc")
+        .collection('todos')
+        .orderBy('timestamp', 'desc')
         .onSnapshot((snapshot) => {
           setTodos(snapshot.docs.map((doc) => ({id: doc.id, todo: doc.data().todo})))
         });
     }, [todos]);
     
     const addTodo = (event) => {
-        event.preventDeafult();
-    }
+      event.preventDefault();
+      console.log("Add todo clicked");
+      event.preventDefault();
+      db.collection("projects").doc(props.projectID).collection("todos").add({
+        todo: todo,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      });
+      setTodo("");
+    };
 
 
     return (
@@ -33,8 +41,8 @@ function Project(props) {
         {/* list of todos */}
         <List>
           {/* map todos */}
-          {[1, 2, 3, 4].map((value) => (
-            <Todo key={`Item ${value}`} value={`Item ${value}`}/>
+          {todos.map((todo) => (
+            <Todo key={todo.id} value={todo.todo}/>
           ))}
         </List>
 
@@ -55,7 +63,7 @@ function Project(props) {
             onClick={addTodo}
             className="project__button"
           >
-            <ArrowForwardIcon className="project__arrowIcon" />
+            Add todo{/* <ArrowForwardIcon className="project__arrowIcon" /> */}
           </Button>
         </form>
       </div>
