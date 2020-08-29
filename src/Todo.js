110e47b88ad20179074c22ec931520a8794f3227
Abrahamcepe <abrahamgolf@gmail.com>
2020-08-29 13:51:24 -0500

    Customized  checkbox

--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { ListItem, ListItemIcon, ListItemText, ListItemSecondaryAction } from "@material-ui/core";
+import { withStyles } from '@material-ui/core/styles';
+import { green } from '@material-ui/core/colors';
 import Checkbox from "@material-ui/core/Checkbox";
 import IconButton from "@material-ui/core/IconButton";
 import DeleteIcon from '@material-ui/icons/Delete';
 import './css/Todo.css';
 import { db } from './firebase';
+
+const MyCheckbox = withStyles({
+  root: {
+    color: 'white',
+    '&$checked': {
+      color: 'white',
+    },
+  },
+  checked: {},
+})((props) => <Checkbox color="default" {...props} />);
 
 function Todo(props) {
     const [checked, setChecked] = React.useState([0]);
@@ -29,12 +41,13 @@ function Todo(props) {
     return (
       <ListItem onClick={handleToggle(props.value)}>
         <ListItemIcon>
-          <Checkbox
+          <MyCheckbox edge="start" checked={checked.indexOf(props.value) !== -1} tabIndex={-1}/>
+          {/* <Checkbox
             edge="start"
             checked={checked.indexOf(props.value) !== -1}
             tabIndex={-1}
             disableRipple
-          />
+          /> */}
         </ListItemIcon>
         <ListItemText primary={props.value} />
         <ListItemSecondaryAction>
